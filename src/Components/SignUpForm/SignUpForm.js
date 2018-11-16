75828f1878282b5e89a67b4593d289b8846349b2
Long Zhao <zlong@outlook.com>
2018-11-16 20:18:05 +1100

    SignUpForm UI

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import logo from '../../images/logo.svg';
+import './SignUpForm.css';
 
 const SignUpForm = () => (
-  <div>SignUpForm</div>
+  <div className="signUpFormContainer">
+    <form className="signUpForm">
+      <div className="imageContainer">
+        <img className="signUpLogo" src={logo} alt="SIGN UP" />
+      </div>
+      <div className="inputsContainer">
+        <input className="email" type="text" placeholder="Email" />
+        <input className="password" type="password" placeholder="Password" />
+        <input className="confirmPassword" type="password" placeholder="Confirm password" />
+      </div>
+      <div className="submitContainer">
+        <button type="submit">Sign Up</button>
+      </div>
+    </form>
+  </div>
 );
 
 export default SignUpForm;
