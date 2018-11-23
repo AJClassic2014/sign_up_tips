e9496b4c56b1991f137609e9949ad8d366d9fd3a
Long Zhao <zlong@outlook.com>
2018-11-23 18:39:20 +1100

    Sign up form ver 1

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,23 +1,145 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
+import signUp from '../../apis/signUp';
 import './SignUpForm.css';
 
-const SignUpForm = () => (
-  <div className="signUpFormContainer">
-    <form className="signUpForm">
-      <div className="imageContainer">
-        <img className="signUpLogo" src={logo} alt="SIGN UP" />
-      </div>
-      <div className="inputsContainer">
-        <input className="email" type="text" placeholder="Email" />
-        <input className="password" type="password" placeholder="Password" />
-        <input className="confirmPassword" type="password" placeholder="Confirm password" />
-      </div>
-      <div className="submitContainer">
-        <button type="submit">Sign Up</button>
-      </div>
-    </form>
-  </div>
-);
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      errorMessages: [],
+    };
+
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onConfirmPasswordChange = this.onConfirmPasswordChange.bind(this);
+  }
+
+  onEmailChange(value) {
+    this.setState({
+      email: value,
+    })
+  }
+
+  onPasswordChange(value) {
+    this.setState({
+      password: value,
+    })
+  } 
+  
+  onConfirmPasswordChange(value) {
+    this.setState({
+      confirmPassword: value,
+    })
+  }
+
+  setErrorMessages(value) {
+    this.setState({
+      errorMessages: value,
+    });
+  }
+
+  onSubmit() {
+    
+  }
+
+  render() {
+    const { email, password, confirmPassword, errorMessages } = this.state;
+
+    return (
+      <React.Fragment>
+        <div className="signUpFormContainer">
+          <form 
+            className="signUpForm" 
+            onSubmit={(event) => {
+              event.preventDefault();
+              this.setErrorMessages([]);
+
+              let errors = [];
+
+              if (!email) {
+                errors = [...errors, {
+                  className: 'email',
+                  text: 'Please input your email',
+                }];
+              }
+
+              if (!password) {
+                errors = [...errors, {
+                  className: 'password',
+                  text: 'Please input your password',
+                }];
+              }
+
+              if (!confirmPassword) {
+                errors = [...errors, {
+                  className: 'confirmPassword',
+                  text: 'Please input your confirm password',
+                }];
+              }
+
+              if (password !== confirmPassword) {
+                errors = [...errors, {
+                  className: 'differentPasswordError',
+                  text: 'Please input the same password',
+                }];
+              }
+
+              if (errors.length > 0) {
+                this.setErrorMessages(errors);
+                return;
+              }
+
+              signUp();
+            }}
+          >
+            <div className="imageContainer">
+              <img className="signUpLogo" src={logo} alt="SIGN UP" />
+            </div>
+            <div className="inputsContainer">
+              <input 
+                className="email"
+                type="text" 
+                placeholder="Email" 
+                value={email}
+                onChange={({ target: { value } }) => this.onEmailChange(value)}
+              />
+              <input 
+                className="password"
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange={({ target: { value } }) => this.onPasswordChange(value)}
+              />
+              <input 
+                className="confirmPassword"
+                type="password" 
+                placeholder="Confirm password" 
+                value={confirmPassword}
+                onChange={({ target: { value } }) => this.onConfirmPasswordChange(value)}
+              />
+            </div>
+           {errorMessages.length > 0 && (
+              <div className="error">
+                {errorMessages.map(({ className, text }) => (
+                  <p key={className} className={className}>{text}</p>
+                ))}
+              </div>
+            )}
+            <div className="submitContainer">
+              <button type="submit">
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default SignUpForm;
--- src/Components/SignUpForm/SignUpForm.test.js
+++ src/Components/SignUpForm/SignUpForm.test.js
@@ -1,7 +1,128 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import SignUpForm from './SignUpForm';
+import signUp from '../../apis/signUp';
 
-it('render', () => {
-  mount(<SignUpForm />);
-});
+jest.mock('../../apis/signUp', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe('SignUpForm', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<SignUpForm />);
+  });
+
+  it('should input email and set state', () => {
+    wrapper.find('input.email').simulate('change', { target: { value: 'email' } });
+    expect(wrapper.state('email')).toBe('email');
+  });
+
+  it('should input password and set state', () => {
+    wrapper.find('input.password').simulate('change', { target: { value: 'password' } });
+    expect(wrapper.state('password')).toBe('password');
+  });
+
+  it('should input confirmPassword and set state', () => {
+    wrapper.find('input.confirmPassword').simulate('change', { target: { value: 'confirmPassword' } });
+    expect(wrapper.state('confirmPassword')).toBe('confirmPassword');
+  });
+
+  it('should not show error when user first see sign up form', () => {
+    const error = wrapper.find('.error');
+    expect(error.exists()).toBeFalsy();
+  });
+
+  describe('Email input - User input a blank email and submit', () => {
+    beforeEach(() => {
+      wrapper.find('input.email').simulate('change', { target: { value: '' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+    })
+
+    it('should show "Please input your email"', () => {
+      const emailError = wrapper.find('.error .email');
+      expect(emailError.exists()).toBeTruthy();
+      expect(emailError.text()).toBe('Please input your email');
+    });
+
+    it('should hide "Please input your email" after user input email', () =>{
+      wrapper.find('input.email').simulate('change', { target: { value: 'email' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+      const emailError = wrapper.find('.error .email');
+      expect(emailError.exists()).toBeFalsy();
+    });
+  });
+
+  describe('Password input - User input a blank password and submit', () => {
+    beforeEach(() => {
+      wrapper.find('input.password').simulate('change', { target: { value: '' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+    })
+
+    it('should show "Please input your password"', () => {
+      const passwordError = wrapper.find('.error .password');
+      expect(passwordError.exists()).toBeTruthy();
+      expect(passwordError.text()).toBe('Please input your password');
+    });
+
+    it('should hide "Please input your password" after user input password', () =>{
+      wrapper.find('input.password').simulate('change', { target: { value: 'password' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+      const passwordError = wrapper.find('.error .password');
+      expect(passwordError.exists()).toBeFalsy();
+    });
+  });
+
+  describe('Confirm password input - User input a blank confirm password and submit', () => {
+    beforeEach(() => {
+      wrapper.find('input.confirmPassword').simulate('change', { target: { value: '' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+    })
+
+    it('should show "Please input your confirmPassword"', () => {
+      const confirmPasswordError = wrapper.find('.error .confirmPassword');
+      expect(confirmPasswordError.exists()).toBeTruthy();
+      expect(confirmPasswordError.text()).toBe('Please input your confirm password');
+    });
+
+    it('should hide "Please input your confirmPassword" after user input confirmPassword', () =>{
+      wrapper.find('input.confirmPassword').simulate('change', { target: { value: 'confirmPassword' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+      const confirmPasswordError = wrapper.find('.error .confirmPassword');
+      expect(confirmPasswordError.exists()).toBeFalsy();
+    });
+  });
+
+  describe('Password equals to Confirm password', () => {
+    beforeEach(() => {
+      wrapper.find('input.confirmPassword').simulate('change', { target: { value: 'password' } });
+      wrapper.find('input.password').simulate('change', { target: { value: 'confirmPassword' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+    });
+
+    it('should show "Please input the same password" after user input confirm password', () => {
+      const differentPasswordError = wrapper.find('.error .differentPasswordError');
+      expect(differentPasswordError.exists()).toBeTruthy();
+    });
+
+    it('should not show "Please input the same password" after user input confirm password', () => {
+      wrapper.find('input.confirmPassword').simulate('change', { target: { value: 'password' } });
+      wrapper.find('input.password').simulate('change', { target: { value: 'password' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+      const differentPasswordError = wrapper.find('.error .differentPasswordError');
+      expect(differentPasswordError.exists()).toBeFalsy();
+    });
+  });
+
+  describe('Submit', () => {
+    it('should call submit', () => {
+      wrapper.find('input.email').simulate('change', { target: { value: 'email' } });
+      wrapper.find('input.confirmPassword').simulate('change', { target: { value: 'password' } });
+      wrapper.find('input.password').simulate('change', { target: { value: 'password' } });
+      wrapper.find('form.signUpForm').simulate('submit');
+      expect(signUp).toBeCalled();
+    });
+  });
+})
